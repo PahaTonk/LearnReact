681baedb24a79f26b84c22f3e32a56ace6ec8835
Паша <gavrikboss@gmail.com>
2019-03-14 23:56:45 +0300

    Events React

--- src/index.js
+++ src/index.js
@@ -2,56 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const INTERVAL = 100;
-
-class Timer extends React.Component {
-  constructor (props) {
+class ClickButton extends React.Component {
+  constructor(props) {
     super(props);
-    this.state = {value: 0}
-  }
+    this.state = {class: "off", label: "Click", style: {"backgroundColor":"red"}};
 
-  increment() {
-    this.setState({value: this.state.value + 1});
+    this.press = this.press.bind(this);
   }
-
-  componentDidMount() {
-    this.timerID = setInterval(() => this.increment(), 1000/INTERVAL);
+  press (e) {
+    console.log(e);
+    let className = this.state.class === "off" ? "on" : "off";
+    let styleBut = className === "on" ? {"backgroundColor":"green"} : {"backgroundColor":"red"};
+    this.setState({style: styleBut});
+    this.setState({class: className});
   }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  render () {
-    const value = this.state.value;
+  render() {
     return (
       <div>
-        <p>Таймер:</p>
-        <ClockValue value={value} />
+      <button onClick={this.press} className={this.state.class} style={this.state.style}>{this.state.label}</button>
+      <CountNumber label={`${this.state.label} Count`} />
       </div>
-    );
+    )
   }
 }
-
-function ClockValue(props) {
-  const value = props.value;
-  return (
-    <p>
-      <span>{Math.round(value/INTERVAL/60/60)} : </span>
-      <span>{Math.round(value/INTERVAL/60)} : </span>
-      <span>{Math.round(value/INTERVAL)} . </span>
-      <span>{value % INTERVAL}</span>
-    </p>
-  )
+class CountNumber extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {value: 0};
+    this.pressBut = this.pressBut.bind(this);
+  }
+  pressBut () {
+    console.log(this.state.value);
+    this.setState({value: this.state.value+1});
+    console.log(this.state.value);
+  }
+  render () {
+    return(
+      <div>
+        <h2 style={{color:'blue',fontSize: '15px'}}>{this.state.value}</h2>
+        <button onClick={this.pressBut}>{this.props.label}</button>
+      </div>
+    )
+  }
 }
-function Application() {
-  return (
-    <p>
-      <Timer/>
-      <Timer/>
-      <Timer/>
-    </p>
-  );
-}
-  ReactDOM.render(<Application />,  document.getElementById('root'));
-//2.6.5 Нисходящий поток данных https://learn-reactjs.ru/basics/state-and-lifecycle
+  ReactDOM.render(<ClickButton />,  document.getElementById('root'));
