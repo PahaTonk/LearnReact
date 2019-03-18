ac2967dcccbe959827e26fa8f75010ea88b0d658
Паша <gavrikboss@gmail.com>
2019-03-18 19:07:01 +0300

    Условный рендеринг 2 часть

--- src/export/Lesson.jsx
+++ src/export/Lesson.jsx
@@ -1,22 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 const ValidationMsg = ({ val }) => {
-  if(val >= 10) {
-    return <h2>Good</h2>;
+  if (val === 1) {
+    return <h2>Tab1</h2>;
+  } else if (val === 2) {
+    return <h2>Tab2</h2>;
   } else {
-    return <h3>{`Bad. ${val} < 10 `}</h3>;
+    return <h2>Tab3</h2>;
   }
 }
 
-const App = () => {
-  const val = 8;
-  return (
-    <div>
-      <ValidationMsg val={8} />
-      {val >= 10 ? <h2>Good</h2> : <h3>{`Bad. ${val} < 10 `}</h3>}
-      {val && <h2>{`${val} is true`}</h2>}
-    </div>
-  )
+ValidationMsg.propTypes = {
+  val : PropTypes.number.isRequired,
+}
+
+class App extends Component {
+  state = {
+    name : 1,
+  }
+  handleTab = (e) => {
+    this.setState({
+      name: +e.target.dataset.name,
+    });
+  }
+  render() {
+    const arr = ['Tab1', 'Tab2', 'Tab3'];
+    return (
+      <Fragment>
+        {arr.map( (title, index) =>  {
+          return <button data-name={index+1} key={`${index+1}key`} onClick={this.handleTab}>{title}</button>
+        } )}
+        <ValidationMsg val={this.state.name} />
+      </Fragment>
+    );
+  }
 }
 export default App;
