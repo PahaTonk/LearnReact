fde377876c86c09cb94da441d36789b32aa7609d
Паша <gavrikboss@gmail.com>
2019-03-18 19:20:07 +0300

    Условный рендеринг 3 часть

--- src/export/Lesson.jsx
+++ src/export/Lesson.jsx
@@ -1,37 +1,39 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 
-const ValidationMsg = ({ val }) => {
-  if (val === 1) {
-    return <h2>Tab1</h2>;
-  } else if (val === 2) {
-    return <h2>Tab2</h2>;
-  } else {
-    return <h2>Tab3</h2>;
-  }
+const Tab1 = () => {
+  return <h2>Tab1</h2>;
 }
 
-ValidationMsg.propTypes = {
-  val : PropTypes.number.isRequired,
+const Tab2 = () => {
+  return <h2>Tab2</h2>;
+}
+
+const Tab3 = () => {
+  return <h2>Tab3</h2>;
 }
 
 class App extends Component {
   state = {
-    name : 1,
+    activeTab : 1,
   }
   handleTab = (e) => {
     this.setState({
-      name: +e.target.dataset.name,
+      activeTab: +e.target.dataset.name,
     });
   }
   render() {
+    const { activeTab } = this.state;
     const arr = ['Tab1', 'Tab2', 'Tab3'];
     return (
       <Fragment>
         {arr.map( (title, index) =>  {
           return <button data-name={index+1} key={`${index+1}key`} onClick={this.handleTab}>{title}</button>
         } )}
-        <ValidationMsg val={this.state.name} />
+        {activeTab === 1 && <Tab1 />}
+        {activeTab === 2 && <Tab2 />}
+        {activeTab === 3 && <Tab3 />}
+        {activeTab === 1 ? <Tab1 /> : activeTab === 2 ? <Tab2 /> : <Tab3 />}
       </Fragment>
     );
   }
