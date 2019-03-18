e0d9187ffe33d3e065de6ce0e8be6739c0676d33
Паша <gavrikboss@gmail.com>
2019-03-18 23:26:29 +0300

    HOC

--- src/export/Lesson.jsx
+++ src/export/Lesson.jsx
@@ -1,78 +1,25 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
-class Form extends Component {
-  state = {
-    inputText: '',
-    textareaText: '',
-    selectText: '',
-    showData: {
-      name: '',
-      text: '',
-      position: '',
-    }
-  }
+const AppLink = (props) => ({
+  render: () => (
+    <Link {...props} activeClassName="active" />
+  )
+})
 
-  getRef = node => this.el = node;
-  textareaRef = React.createRef();
-  selectRef = React.createRef();
-
-  handleChange = ({ target: { value } }) => {
-    this.setState({
-      inputText: this.el.value,
-      textareaText: this.textareaRef.current.value,
-      selectText: this.selectRef.current.value
-   });
-  }
-
-
-  handleShow = (e) => {
-    e.preventDefault();
-    const { inputText, textareaText, selectText } = this.state;
-    this.setState({
-      inputText: '',
-      textareaText: '',
-      selectText: '',
-      showData: {
-        name: inputText,
-        text: textareaText,
-        position: selectText,
-      }
-    });
-  }
-
+class Lesson extends Component {
   render() {
-    const { inputText, textareaText, selectText, showData } = this.state;
-    const { name, text, position } = showData;
     return (
-      <Fragment>
-          <form>
-            <label>
-              Name:<br />
-              <input type="text" name="name"
-               value={inputText}
-               ref={this.getRef}
-               onChange={this.handleChange} />
-            </label><br />
-            <label htmlFor="text">Text:</label>
-            <textarea id="text"
-              value={textareaText}
-              ref={this.textareaRef}
-              onChange={this.handleChange} /><br />
-            <select value={selectText}
-              ref={this.selectRef}
-              onChange={this.handleChange}>
-              <option value="Front-end">Front-end</option>
-              <option value="Back-end">Back-end</option>
-            </select>
-            <button onClick={this.handleShow}>Show</button>
-          </form>
-          {name && <h2>{name}</h2>}
-          {text && <h2>{text}</h2>}
-          {position && <h2>{position}</h2>}
-      </Fragment>
+      <Router>
+        <nav>
+          <AppLink to="/">Home</AppLink>
+          <AppLink to="/portfolio">Portfolio</AppLink>
+          <AppLink to="/contacts">Contacts</AppLink>
+        </nav>
+      </Router>
     );
   }
 }
 
-export default Form;
+export default Lesson;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import Form from './export/Lesson.jsx'
+import Lesson from './export/Lesson.jsx'
 import './index.css';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
-ReactDOM.render(<Form />,  document.getElementById('root'));
+ReactDOM.render(<Lesson />,  document.getElementById('root'));
