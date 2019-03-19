341d2e6120c4fc8c378825707dcb76794fe870d6
Паша <gavrikboss@gmail.com>
2019-03-19 15:14:44 +0300

    Mini preloader

--- src/export/Lesson.jsx
+++ src/export/Lesson.jsx
@@ -1,23 +1,35 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
 
-const AppLink = (props) => ({
-  render: () => (
-    <Link {...props} activeClassName="active" />
-  )
-})
+const Preloader = () => {
+  return <h1>Preloader</h1>
+}
+
+const AddHello = ({ data : { text } }) => {
+  return <h1>{text}</h1>
+}
 
 class Lesson extends Component {
+  state = {
+    data : {}
+  }
+
+  answerServer = () => {
+    setTimeout( () => {
+      this.setState({ data : {
+        load: true,
+        text: 'Hello!'
+      } });
+    }, 3000 );
+  }
+
   render() {
+    const { data } = this.state;
+    this.answerServer();
     return (
-      <Router>
-        <nav>
-          <AppLink to="/">Home</AppLink>
-          <AppLink to="/portfolio">Portfolio</AppLink>
-          <AppLink to="/contacts">Contacts</AppLink>
-        </nav>
-      </Router>
+      <Fragment>
+        {!Object.keys(data).length ? <Preloader /> : <AddHello {...this.state} />}
+      </Fragment>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Lesson from './export/Lesson.jsx'
 import './index.css';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 ReactDOM.render(<Lesson />,  document.getElementById('root'));
